d471ad1d62dbf0a73a01ec6a288ddc5c114b1fab
rileymountney <42170367+rileymountney@users.noreply.github.com>
2018-09-18 15:19:03 +1000

    Add files via upload
    fixed all the errors in the highscorecontroller.cs

--- srcC#/HighScoreController.cs
+++ srcC#/HighScoreController.cs
@@ -1,5 +1,9 @@
+using Microsoft.VisualBasic;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
 using System.IO;
 using SwinGameSDK;
 
@@ -16,8 +20,8 @@ using SwinGameSDK;
 /// ''' </remarks>
 static class HighScoreController
 {
-    private const static int NAME_WIDTH = 3;
-    private const static int SCORES_LEFT = 490;
+    private const int NAME_WIDTH = 3;
+    private const int SCORES_LEFT = 490;
 
     /// <summary>
     ///     ''' The score structure is used to keep the name and
@@ -126,7 +130,7 @@ static class HighScoreController
         if (_Scores.Count == 0)
             LoadScores();
 
-        SwinGame.DrawText("   High Scores   ", Color.White, GameFont("Courier"), SCORES_LEFT, SCORES_HEADING);
+        SwinGame.DrawText("   High Scores   ", Color.White, GameResources.GameFont("Courier"), SCORES_LEFT, SCORES_HEADING);
 
         // For all of the scores
         int i;
@@ -134,13 +138,13 @@ static class HighScoreController
         {
             Score s;
 
-            s = _Scores.Item[i];
+            s = _Scores[i];
 
             // for scores 1 - 9 use 01 - 09
             if (i < 9)
-                SwinGame.DrawText(" " + (i + 1) + ":   " + s.Name + "   " + s.Value, Color.White, GameFont("Courier"), SCORES_LEFT, SCORES_TOP + i * SCORE_GAP);
+                SwinGame.DrawText(" " + (i + 1) + ":   " + s.Name + "   " + s.Value, Color.White, GameResources.GameFont("Courier"), SCORES_LEFT, SCORES_TOP + i * SCORE_GAP);
             else
-                SwinGame.DrawText(i + 1 + ":   " + s.Name + "   " + s.Value, Color.White, GameFont("Courier"), SCORES_LEFT, SCORES_TOP + i * SCORE_GAP);
+                SwinGame.DrawText(i + 1 + ":   " + s.Name + "   " + s.Value, Color.White, GameResources.GameFont("Courier"), SCORES_LEFT, SCORES_TOP + i * SCORE_GAP);
         }
     }
 
@@ -151,7 +155,7 @@ static class HighScoreController
     public static void HandleHighScoreInput()
     {
         if (SwinGame.MouseClicked(MouseButton.LeftButton) || SwinGame.KeyTyped(KeyCode.vk_ESCAPE) || SwinGame.KeyTyped(KeyCode.vk_RETURN))
-            EndCurrentState();
+            GameController.EndCurrentState();
     }
 
     /// <summary>
@@ -169,26 +173,26 @@ static class HighScoreController
             LoadScores();
 
         // is it a high score
-        if (value > _Scores.Item[_Scores.Count - 1].Value)
+        if (value > _Scores[_Scores.Count - 1].Value)
         {
             Score s = new Score();
             s.Value = value;
 
-            AddNewState(GameState.ViewingHighScores);
+            GameController.AddNewState(GameState.ViewingHighScores);
 
             int x;
-            x = SCORES_LEFT + SwinGame.TextWidth(GameFont("Courier"), "Name: ");
+            x = SCORES_LEFT + SwinGame.TextWidth(GameResources.GameFont("Courier"), "Name: ");
 
-            SwinGame.StartReadingText(Color.White, NAME_WIDTH, GameFont("Courier"), x, ENTRY_TOP);
+            SwinGame.StartReadingText(Color.White, NAME_WIDTH, GameResources.GameFont("Courier"), x, ENTRY_TOP);
 
             // Read the text from the user
             while (SwinGame.ReadingText())
             {
                 SwinGame.ProcessEvents();
 
-                DrawBackground();
+                UtilityFunctions.DrawBackground();
                 DrawHighScores();
-                SwinGame.DrawText("Name: ", Color.White, GameFont("Courier"), SCORES_LEFT, ENTRY_TOP);
+                SwinGame.DrawText("Name: ", Color.White, GameResources.GameFont("Courier"), SCORES_LEFT, ENTRY_TOP);
                 SwinGame.RefreshScreen();
             }
 
@@ -201,7 +205,7 @@ static class HighScoreController
             _Scores.Add(s);
             _Scores.Sort();
 
-            EndCurrentState();
+            GameController.EndCurrentState();
         }
     }
 }
